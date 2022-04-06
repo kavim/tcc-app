a37af13fb2ec597534f7530e3c53e0522e1c0ee3
kavim <kmmtmg@gmail.com>
2022-04-06 17:42:02 -0300

    minorFix

--- app/Http/Controllers/SocialiteLoginController.php
+++ app/Http/Controllers/SocialiteLoginController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\Student;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Laravel\Socialite\Facades\Socialite;
