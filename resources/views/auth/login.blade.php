0dcd900279c574a4b7403a30f5b6ab6fe63c7ae0
kavim <kmmtmg@gmail.com>
2022-04-08 15:45:24 -0300

    fix: tradução

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -60,31 +60,10 @@
                                 </div>
                             </div>
 
-                            {{-- <div class="row mb-3">
-                                <div class="col-md-6 offset-md-4">
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" name="remember" id="remember"
-                                            {{ old('remember') ? 'checked' : '' }}>
-
-                                        <label class="form-check-label" for="remember">
-                                            {{ __('Remember Me') }}
-                                        </label>
-                                    </div>
-                                </div>
-                            </div> --}}
-
-                            {{-- <div class="form-group row mb-0">
-                                <div class="col-md-8 offset-md-4">
-                                    <a href="{{ route('linkedinlogin', 'linkedin') }}">
-                                        {{ __('Login with LinkedIn') }}
-                                    </a>
-                                </div>
-                            </div> --}}
-
                             <div class="row mb-0">
                                 <div class="col-md-8 offset-md-4">
                                     <button type="submit" class="btn btn-primary btn-lg btn-block">
-                                        {{ __('Login') }}
+                                        {{ __('sentences.login') }}
                                     </button>
 
                                     @if (Route::has('password.request'))
