631c7fc93570811e489d9df0209cacfa9ea5d573
kavim <kmmtmg@gmail.com>
2022-04-08 15:44:30 -0300

    fix: tradução

--- resources/views/web/for_companies.blade.php
+++ resources/views/web/for_companies.blade.php
@@ -205,7 +205,7 @@
 
                                             @if (Route::has('password.request'))
                                                 <a class="btn btn-link" href="{{ route('password.request') }}">
-                                                    {{ __('Forgot Your Password?') }}
+                                                    {{ __('sentences.forgot_password') }}
                                                 </a>
                                             @endif
                                         </div>
